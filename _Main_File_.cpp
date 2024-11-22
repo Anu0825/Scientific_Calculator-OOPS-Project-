cacd3a46fb90b931942900c4feb0a59f2f45b6de
Manharan Kaur <thisismanharankaur@gmail.com>
2024-11-22 20:31:28 +0530

    Changed main file

---
+++ _Main_File_.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Complex.h"
+#include "Fraction.h"
+#include "Matrix.h"
+#include "Polynomial.h"
+#include "VectorOperations.h"
+#include "Calculator.h"
+#include <cmath>
+#include <map>
+#include <string>
+
+
+
+int main()
+{
+    // Main function is currently cleared for you to think about how to implement the Scientific Calculator class
+    // Feel free to proceed with your design and later implement the functionality
+}
